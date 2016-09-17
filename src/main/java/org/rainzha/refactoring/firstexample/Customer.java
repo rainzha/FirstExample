acc3ff3e4b79351525fec5fa45248c904bbf4a7c
rainzha <rainzha@163.com>
2016-09-17 11:29:07 +0800

    添加htmlStatement()函数。

--- src/main/java/org/rainzha/refactoring/firstexample/Customer.java
+++ src/main/java/org/rainzha/refactoring/firstexample/Customer.java
@@ -39,6 +39,27 @@ class Customer {
         return result;
     }
 
+    public String htmlStatement() {
+        Enumeration rentals = _rentals.elements();
+        String result = "<h1>Rentals for <em>" + getName() +
+                "</em></h1><p/>\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+
+            result += each.getMovie().getTitle() + ": " +
+                    String.valueOf(each.getCharge()) + "<br/>\n";
+        }
+
+        result += "<p>You owe <em>" +
+                String.valueOf(getTotalCharge()) +
+                "</em></p>\n";
+        result += "On this rental you earned <em>" +
+                String.valueOf(getTotalFrequentRenterPoints()) +
+                "</em> frequent renter points<p/>";
+
+        return result;
+    }
+
     private double getTotalCharge() {
         double result = 0;
         Enumeration rentals = _rentals.elements();
