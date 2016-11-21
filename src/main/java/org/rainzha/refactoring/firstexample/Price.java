2481d5ed9313524958cb922dfec7b18926a54627
rainzha <rainzha@163.com>
2016-11-21 15:55:59 +0800

    运用Replace Conditional with Polymorphism.

--- src/main/java/org/rainzha/refactoring/firstexample/Price.java
+++ src/main/java/org/rainzha/refactoring/firstexample/Price.java
@@ -4,25 +4,7 @@ package org.rainzha.refactoring.firstexample;
 abstract class Price {
     abstract int getPriceCode();
 
-    double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
+    abstract double getCharge(int daysRented);
 }
 
 
@@ -32,6 +14,15 @@ class ChildrensPrice extends Price {
     int getPriceCode() {
         return Movie.CHILDRENS;
     }
+
+    @Override
+    double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+
+        return result;
+    }
 }
 
 class NewReleasePrice extends Price {
@@ -40,6 +31,11 @@ class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
 
 class RegularPrice extends Price {
@@ -47,5 +43,14 @@ class RegularPrice extends Price {
     @Override
     int getPriceCode() {
         return Movie.REGULAR;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+
+        return result;
     }
 }
