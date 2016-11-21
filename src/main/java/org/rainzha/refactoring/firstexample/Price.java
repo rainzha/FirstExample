58c9b8bf01e35321ec69f7699b4ac71e77202be6
rainzha <rainzha@163.com>
2016-11-21 15:44:42 +0800

    使用Replace Type Code with State/Strategy.

---
+++ src/main/java/org/rainzha/refactoring/firstexample/Price.java
@@ -0,0 +1,31 @@
+package org.rainzha.refactoring.firstexample;
+
+
+abstract class Price {
+    abstract int getPriceCode();
+}
+
+
+class ChildrensPrice extends Price {
+
+    @Override
+    int getPriceCode() {
+        return Movie.CHILDRENS;
+    }
+}
+
+class NewReleasePrice extends Price {
+
+    @Override
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+}
+
+class RegularPrice extends Price {
+
+    @Override
+    int getPriceCode() {
+        return Movie.REGULAR;
+    }
+}
